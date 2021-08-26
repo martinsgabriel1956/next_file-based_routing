17bbb0d23de1f9f722877f716a0f3eb9845f5187
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-08-26 17:43:57 -0300

    feat: navigation with link component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h2>Home</h2>;
+  return (
+    <>
+      <h1>Home</h1>
+      <div>
+        <Link href="/portfolio">Portfolio</Link>
+      </div>
+
+      <div>
+        <Link href="/clients">Clients</Link>
+      </div>
+    </>
+  );
 }
