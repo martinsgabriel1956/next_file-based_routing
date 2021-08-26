b459e9aced232c96ad46d09fbaec8162b60c72a1
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-08-26 14:55:37 -0300

    feat: extracting dynamic path segment data in the slug page

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,3 +1,9 @@
+import { useRouter } from 'next/router';
+
 export default function PortfolioProject() {
+  const router = useRouter();
+  console.log(router.pathname);
+  console.log(router.query);
+
   return <h2>PortfolioProject</h2>;
 }
