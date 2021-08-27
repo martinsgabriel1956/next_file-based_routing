94827c8cfd3762781fd9ad989a457544fa8e3f1a
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-08-27 11:09:22 -0300

    feat: Creating navigation programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,8 +1,21 @@
-import { useRouter } from 'next/router';
+import { route } from "next/dist/server/router";
+import { useRouter } from "next/router";
 
 export default function ClientsProjects() {
   const router = useRouter();
-  console.log(router.query)
+  console.log(router.query);
 
-  return <h2>ClientsProjects</h2>;
+  function handleLoadProject() {
+    router.push({
+      pathname: '/clients/gabriel/projecta',
+      query: {id: 'Gabriel', clientproject_id: 'projecta'}
+    });
+  }
+
+  return (
+    <>
+      <h2>ClientsProjects</h2>
+      <button onClick={handleLoadProject}>Load Project A</button>
+    </>
+  );
 }
